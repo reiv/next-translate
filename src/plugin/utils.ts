92d3ed90b5bd872ca754a20cb9d912ca28a6c409
reiv <reiv@users.noreply.github.com>
2022-08-08 10:06:48 +0200

    Stub out hasHOC

--- src/plugin/utils.ts
+++ src/plugin/utils.ts
@@ -52,60 +52,7 @@ export function isPageToIgnore(pageFilePath: string) {
 }
 
 export function hasHOC(rawData: string) {
-  const hasWithTranslationHOC = new RegExp(
-    'import *(\\w*) *.*from *.*next-translate\\/withTranslation.*'
-  )
-
-  if (!rawData.includes('export default')) return false
-  if (
-    hasExportName(rawData, 'getStaticProps') ||
-    hasExportName(rawData, 'getServerSideProps') ||
-    hasExportName(rawData, 'getStaticPaths')
-  ) {
-    return false
-  }
-
-  const [, withTranslationName] = rawData.match(hasWithTranslationHOC) || []
-  const data = rawData
-    // Remove withTranslation hoc, in this case we can ensure that is not using
-    // a getInitialProps on the Page.
-    // Ex: "export default withTranslation(somevariable)" -> export default somevariable
-    .replace(new RegExp(`${withTranslationName}\\(.*\\)`), (d: string) =>
-      d.replace(new RegExp(`(${withTranslationName}|\\(|\\))`, 'g'), '')
-    )
-    // Clear all comments
-    .replace(clearCommentsRgx, '')
-
-  // If is exported normally (function, var, etc), is not a HOC
-  const exportedNormally = new RegExp(
-    `export default (\\(.*\\) *=>|function|class)`
-  ).test(data)
-  if (exportedNormally) return false
-
-  const ref = getRef(data)
-
-  // If the ref includes a "(", is a HOC
-  if (ref.includes('(')) return true
-
-  // If not, the export default is just a reference defined on other place.
-  // So let's look all the lines that include the reference
-  return (
-    data.split('\n').filter((line: string) => {
-      const isRefLine = line.includes(ref) && !/export +default/.test(line)
-      const isComp = new RegExp(`(function|class) +${ref}\\W`).test(line)
-      const isCompInVar = new RegExp(` *${ref} += +(function|class) +`).test(
-        line
-      )
-      const isArrowFunc = new RegExp(` *${ref}(: *\\w+ *)? += +\\(.*=>`).test(
-        line
-      )
-      const isPotentialHOC = /=.*\(/.test(line)
-
-      return (
-        isRefLine && !isComp && !isCompInVar && !isArrowFunc && isPotentialHOC
-      )
-    }).length > 0
-  )
+  return false
 }
 
 function getRef(data: string) {
